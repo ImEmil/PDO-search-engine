b5f36d006bda51d0ea012f52ce42066fe0834c3f
ImEmil <emil_nilsson_96@live.se>
2014-01-09 16:11:15 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+  require("app/engine.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <title>PDO search engine</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+
+  <header>
+      <h1>PDO search engine</h1>
+  </header>
+
+  <main>
+    <section>
+
+    <?php
+      $keyword = null;
+      $count = 0;
+      if($s->get("key")) {
+      $keyword = clean($_GET['keyword']);
+      $found   = $cms->search($keyword);
+      $count   = count($found);
+    }
+    ?>
+
+    <aside found="<?php echo $count ." results"; ?>"> You searched for [ <?php echo $keyword; ?> ] </aside>
+
+    <?php
+     if($s->get("key") && $count > 0) {
+
+      foreach( $found as $row ) {
+      echo "<article> #{$row->id}  &raquo; {$row->username} </article>";
+    }
+  }
+?>
+    </section>
+  </main>
+
+  <footer> PHP search Engine (PDO OOP) By <a href="https://github.com/ImEmil/" target="_blank">ImEmil</a> </footer>
+
+</body>
+</html>
