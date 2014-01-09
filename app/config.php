52e926436e736d1b847f5912a2b5f62b50dee3b5
ImEmil <emil_nilsson_96@live.se>
2014-01-09 15:56:28 +0100

    Create config.php

---
+++ app/config.php
@@ -0,0 +1,13 @@
+<?php
+/*
+
+                                                   +-+-+-+-+-+-+
+                                                   |I|m|E|m|i|l|
+                                                   +-+-+-+-+-+-+
+                                         
+*/
+
+  define("host", "localhost");
+  define("username", "root");
+  define("password", "mypass123");
+  define("dbname", "githubsearch");
