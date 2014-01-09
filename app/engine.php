d3f7d77604162be0b09ba36664da5fd5d3b3902b
ImEmil <emil_nilsson_96@live.se>
2014-01-09 15:57:35 +0100

    Create engine.php

---
+++ app/engine.php
@@ -0,0 +1,19 @@
+<?php
+/*
+
+                                                   +-+-+-+-+-+-+
+                                                   |I|m|E|m|i|l|
+                                                   +-+-+-+-+-+-+
+                                         
+*/
+
+  function clean($string) {
+    return trim(strip_tags(htmlspecialchars($string, ENT_QUOTES, 'UTF-8')));
+  }
+
+
+  require('app/config.php');
+  require('classes/class.config.php');
+  require('classes/class.search.php');
+  
+  $s        = new Search($s);
