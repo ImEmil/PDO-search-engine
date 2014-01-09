eec2995c257daffdbe710120ede60e81acc5eb16
ImEmil <emil_nilsson_96@live.se>
2014-01-09 15:52:14 +0100

    Update class.search.php

--- app/classes/class.search.php
+++ app/classes/class.search.php
@@ -2,16 +2,22 @@
 class Search{
  
 	private $s;
- 
-	public function __construct($database) {
-	    $this->s = $database;
-	}
+
+  public function __construct($database) {
+	$this->s = $database;
+    }
 	
   public function search($keyword) {
-	    $exc = $this->s->prepare("SELECT * FROM  `users` WHERE `username` LIKE ?");
-	    
-	    $exc->execute( ['%' . $keyword . '%']);         // Executes an array |>  <> Require PHP >= 5.4.0 
-	    
-	    return $exc->fetch(PDO::FETCH_OBJ);
-	}
+	$exc = $this->s->prepare("SELECT * FROM  `users` WHERE `username` LIKE ?");
+	$exc->execute( ['%' . $keyword . '%']);         // Executes an array |>  <> Require PHP >= 5.4.0 
+	return $exc->fetch(PDO::FETCH_OBJ);
+    }
+  
+  public function form($form) {
+   	return (isset($_POST["{$form}"]) ? true : false);                                                
+    }
+
+  public function __destruct() {
+    $this->s = null;
+  }
 }
