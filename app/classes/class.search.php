649a1df53adb408aa1805f9766bd86d1fc8898fa
ImEmil <emil_nilsson_96@live.se>
2014-01-09 15:55:27 +0100

    Update class.search.php

--- app/classes/class.search.php
+++ app/classes/class.search.php
@@ -10,7 +10,13 @@ class Search{
   public function search($keyword) {
 	$exc = $this->s->prepare("SELECT * FROM  `users` WHERE `username` LIKE ?");
 	$exc->execute( ['%' . $keyword . '%']);         // Executes an array |>  <> Require PHP >= 5.4.0 
-	return $exc->fetch(PDO::FETCH_OBJ);
+	
+	if(intval($exc->fetchColumn()) === 0) { // Check if we found something
+          return $exc->fetch(PDO::FETCH_OBJ);
+	}
+	else {
+		return false;
+	}
     }
   
   public function form($form) {
